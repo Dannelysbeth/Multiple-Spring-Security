d00901a75453e3fe24a23cf5cb57741950fdc011
Dannelysbeth <danielaf@onet.pl>
2024-08-20 21:27:04 +0200

    completed multiple login options

--- src/main/java/dannelysbeth/multiple/security/app/config/SecurityConfig.java
+++ src/main/java/dannelysbeth/multiple/security/app/config/SecurityConfig.java
@@ -2,23 +2,58 @@ package dannelysbeth.multiple.security.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
+
     @Bean
+    @Order(1)
+    SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().authenticated();
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(withDefaults())
+                .build();
+    }
+
+
+    @Bean
+    @Order(3)
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.requestMatchers("/error").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin(withDefaults())
+                .build();
+    }
+
+
+    @Bean
+    @Order(2)
     SecurityFilterChain h2ConsoleSecurityConsoleChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth-> {
+                .securityMatcher(AntPathRequestMatcher.antMatcher("/h2-console/**"))
+                .authorizeHttpRequests(auth -> {
                     auth.requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll();
                 })
-                .csrf(csrf->csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")))
-                .headers(headers->headers.frameOptions().disable())
+                .csrf(csrf -> csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")))
+                .headers(headers -> headers.frameOptions().disable())
                 .build();
     }
 }
--- src/main/java/dannelysbeth/multiple/security/app/repository/PostRepository.java
+++ src/main/java/dannelysbeth/multiple/security/app/repository/PostRepository.java
@@ -3,5 +3,5 @@ package dannelysbeth.multiple.security.app.repository;
 import dannelysbeth.multiple.security.app.model.Post;
 import org.springframework.data.repository.ListCrudRepository;
 
-public interface PostRepository extends ListCrudRepository<Post,Integer> {
+public interface PostRepository extends ListCrudRepository<Post, Integer> {
 }
